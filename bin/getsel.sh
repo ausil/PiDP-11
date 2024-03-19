44c5ae62e81efb0d9c13d46a0f92a3eda1aeef33
Dennis Gilmore <dennis@ausil.us>
2024-03-18 23:23:54 -0500

    fix up sed to correctly detect os
    at least here the os selection was not working correctly. breaking out
    the variables has it work correctly
    
    Signed-off-by: Dennis Gilmore <dennis@ausil.us>

--- bin/getsel.sh
+++ bin/getsel.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sys=$1
-awk '$1 == '$sys' { sys = $2; exit } END { if(sys) print sys; else print "default" }' < /opt/pidp11/systems/selections
+switch=$1
+awk '$1 == '$switch' { sys = $2; exit } END { if(sys) print sys; else print "default" }' < /opt/pidp11/systems/selections
