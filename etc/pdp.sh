76106cbed73c70c1483e4f35daf53b4ef9772862
Dennis Gilmore <dennis@ausil.us>
2024-03-19 22:30:12 -0500

    Change the shell escaping
    use $() rather than `` to breakout of the shell
    
    Signed-off-by: Dennis Gilmore <dennis@ausil.us>

--- etc/pdp.sh
+++ etc/pdp.sh
@@ -1,4 +1,4 @@
-procs=`sudo screen -ls pidp11 | egrep '[0-9]+\.pidp11' | wc -l`
+procs=$(sudo screen -ls pidp11 | egrep '[0-9]+\.pidp11' | wc -l)
 echo $procs
 if [ $procs -ne 0 ]; then
 	sudo screen -d -r
